61098e207ac623ec3e5eae37e86902046971106d
Joe Lafiosca <joe@lafiosca.com>
2019-01-14 16:39:56 -0500

    tweak config generation

--- setup/create-config.sh
+++ setup/create-config.sh
@@ -9,11 +9,12 @@ source ./definitions.sh
 echo
 echo "Writing resource stack outputs to config file"
 
-printf '{\n\t"region": "%s",\n\t"userPoolId": "%s",\n\t"userPoolName": "%s",\n\t"appClientId": "%s"\n}' \
+printf '{\n\t"region": "%s",\n\t"userPoolId": "%s",\n\t"userPoolName": "%s",\n\t"userPoolWebClientId": "%s",\n\t"identityPoolId": "%s"\n}' \
 	$(aws configure get region) \
 	$(aws cloudformation describe-stacks --stack-name ${ProjectName} --output text | grep ^OUTPUTS | cut -f3-4 | grep ^UserPoolId | cut -f2) \
 	${UserPoolName} \
 	$(aws cloudformation describe-stacks --stack-name ${ProjectName} --output text | grep ^OUTPUTS | cut -f3-4 | grep ^UserPoolClientWeb | cut -f2) \
+	$(aws cloudformation describe-stacks --stack-name ${ProjectName} --output text | grep ^OUTPUTS | cut -f3-4 | grep ^IdentityPoolId | cut -f2) \
 	> ../config/aws-resources.json
 
 echo "Wrote config file"
